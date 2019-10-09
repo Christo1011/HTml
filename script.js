27d441b9a51c3dc3b85136a631c02e209a4c8421
Steve Spicklemire <steve@spvi.com>
2019-10-09 15:36:52 -0400

    update for 10-9

--- script.js
+++ script.js
@@ -1,47 +1,67 @@
-// get references to the canvas and its context
-var canvas=document.getElementById("canvas");
-var ctx=canvas.getContext("2d");
+var image1 = document.getElementById("image1");
+var canvas1 = document.getElementById("canvas1");
+var ctx = canvas1.getContext('2d');
+var y = 20;
+var x1 = 10;
+var x2 = 300;
+var i;
+for(i = 0; i < 4; i++){
+  ctx.beginPath();
+  ctx.moveTo(x1,y );
+  ctx.lineTo(x2, y);
+  ctx.stroke();
 
-// get the offset position of the canvas
-var $canvas=$("#canvas");
-var Offset=$canvas.offset();
-var offsetX=Offset.left;
-var offsetY=Offset.top;
+  y += 60
+}
 
-var x,y,width,height;
+function allowDrop(ev) {
+  ev.preventDefault();
+}
 
-// select all .tool's
-var $tools=$(".tool");
+function drag(ev) {
+  //ev.dataTransfer.setData("text", ev.target.id);
+  ev.dataTransfer.setData("image/png", ev.target.id);
+}
 
-// make all .tool's draggable
-$tools.draggable({
-    helper:'clone',
-});
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("image/png");
+  //ev.target.appendChild(document.getElementById(data));
+}
+//var x_image = 10;
+//var y_image = 10;
+/*function mouseUp(){
 
+  //y+40
+  ctx.drawImage(image1, x_image, y_image, 30, 30);
+  //x_image += 40
+}*/
+var image_new = new Image(30,30)
+var posx = 10
+var posy = 10
+function getMousePos(canvas,event){
 
-// assign each .tool its index in $tools
-$tools.each(function(index,element){
-    $(this).data("toolsIndex",index);
-});
+  var rect = canvas.getBoundingClientRect(),
+  scaleX = canvas.width / rect.width,
+  scaleY = canvas.height / rect.height;
+  //console.log(rect.width,rect.height);
+  return{
+    x: (event.clientX - rect.left) * scaleX,
+    y: (event.clientY - rect.top) * scaleY
+  };
+}
+function onmouseUp(){
+  image_new.src = 'H.png'
+  /*"draggable="true" ondragstart="drag(event)"*/
+  //var clonedItem = image_new.cloneNode(true);
+  //canvas1.appendChild(clonedItem);
+  //var x_image = event.clientX;
+  //var y_image = event.clientY;
+  var pos = getMousePos(canvas1,event)
+  //console.log(pos)
 
-
-// make the canvas a dropzone
-$canvas.droppable({
-    drop:dragDrop,
-});
-// handle a drop into the canvas
-function dragDrop(e,ui){
-
-    // get the drop point (be sure to adjust for border)
-    x=parseInt(ui.offset.left-offsetX)-1;
-    y=parseInt(ui.offset.top-offsetY);
-    width=ui.helper[0].width;
-    height=ui.helper[0].height;
-
-    // get the drop payload (here the payload is the $tools index)
-    var theIndex=ui.draggable.data("toolsIndex");
-
-    // drawImage at the drop point using the dropped image
-    // This will make the img a permanent part of the canvas content
-    ctx.drawImage($tools[theIndex],x,y,width,height);
+  ctx.drawImage(image_new, pos.x, pos.y, 30, 30);
+  console.log(pos)
+  //posx += 60
+  //posy += 60
 }
